26010ac0f95e80bd7918177c3bba192629462b83
Alexis-aredes <alexis.julian.aredes@gmail.com>
2024-11-26 01:57:37 -0300

    a

--- prueba/script.js
+++ prueba/script.js
@@ -4,21 +4,22 @@ let segundo = document.getElementById ("h2")
 
 let boton = document.getElementById ("boton")
 
+let enlace = document.getElementById ("enlace")
+
 boton.addEventListener("click", () => {
     // Cambiar la visibilidad del primer elemento (titulo)
-    if (titulo.style.display == "none") {
-        titulo.style.display = "block"; // Mostrar el elemento
+    if (titulo.style.display == "none" || titulo.style.display == "") {
+        titulo.style.display = "block"; // Mostrar el titulo
+        segundo.style.display = "block"; // Mostrar el segundo elemento
+        enlace.style.display = "block"; // Mostrar el enlace
         boton.innerText = "Ola"; // Cambiar el texto del botón
     } else {
-        titulo.style.display = "none"; // Ocultar el elemento
+        titulo.style.display = "none"; // Ocultar el titulo
+        segundo.style.display = "none"; // Ocultar el segundo elemento
+        enlace.style.display = "none"; // Ocultar el enlace
         boton.innerText = "Iniciar Juego"; // Restaurar el texto original
     }
-
-    // Cambiar la visibilidad del segundo elemento (segundo)
-    if (segundo.style.display == "none") {
-        segundo.style.display = "block"; // Mostrar el elemento
-    } else {
-        segundo.style.display = "none"; // Ocultar el elemento
-    }
 });
+
+
 
