55c7b3841ba19fed21b0af2c2f04bac07cf1feb6
Axel Villalba <axeluriel.vba@gmail.com>
2024-11-26 00:19:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let boton = document.getElementById("boton1")
 boton.addEventListener("click",() => {
-    if (boton.style.backgroundColor == "rgb(224, 168, 4)") {
+    if (boton.style.backgroundColor === "rgb(224, 168, 4)") {
         boton.style.backgroundColor = "red";
+        boton.innerText = "dea q le pasaba"
     } else {
         boton.style.backgroundColor = "rgb(224, 168, 4)";
+        boton.innerText = "Dale Click Aquí"
     }
     if (boton.style.transform === "scale(1.2)") {
         boton.style.transform = "scale(1)"; // Tamaño original
     } else {
-        boton.style.transform = "scale(1.2)"; // Escala a 1.5 veces su tamaño
+        boton.style.transform = "scale(1.2)"; // Escala a 1.2 veces su tamaño
+    }
+    if (texto) {
+        texto.innerText = "Feliz Cumple Sorete"
+        
+    } else {
+        texto.innerText = "FREE FIRE"
     }
 })
 
 boton.addEventListener("mouseenter",() =>{
     boton.style.backgroundColor = "rgb(224, 168, 4)"
     boton.style.transform = "scale"
+    boton.style.transition = "transform 0.3s ease"
     
 })
 boton.addEventListener("mouseleave",() =>{
@@ -22,4 +31,12 @@ boton.addEventListener("mouseleave",() =>{
     
 })
 let texto = document.getElementById ("texto")
+
+if (texto) {
+    texto.innerText
+    
+} else {
+    
+}
+
 
