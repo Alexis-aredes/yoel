80c4f886cd3c1296fac78290a7691ada3cd132df
Alexis-aredes <alexis.julian.aredes@gmail.com>
2024-11-26 00:43:03 -0300

    a

--- prueba/script.js
+++ prueba/script.js
@@ -5,15 +5,20 @@ let segundo = document.getElementById ("h2")
 let boton = document.getElementById ("boton")
 
 boton.addEventListener("click", () => {
+
     if (titulo.style.display == "none") {
         titulo.style.display = "block"; // Mostrar el elemento
+        boton.innerText == "Iniciar Juego"
+        boton.innerText = "ola"
     } else {
         titulo.style.display = "none"; // Ocultar el elemento
+        boton.innerText = "Iniciar Juego"
     }
-
+    
     if (segundo.style.display == "none") {
         segundo.style.display = "block"; // Mostrar el elemento
     } else {
         segundo.style.display = "none"; // Ocultar el elemento
     }
 });
+
